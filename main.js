453b19fc04f1bb71d6d8eef2906806eb3b4d31e9
lubien <lubien1996@gmail.com>
2020-02-20 23:18:47 -0300

    Progress

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('ola mundo')
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        el: '#app',
+        
+        data() {
+            return {
+                message: 'Hello Vue ficou muito grande',
+                myTitle: 'Voce passou o mouse ali',
+                className: 'spn spn-red',
+                href: 'http://example.org',
+                role: 'admin',
+                tasks: [
+                    { title: 'Learn JavaScript' },
+                    { title: 'Learn Vue' },
+                    { title: 'Build something awesome' }
+                ]
+            }
+        },
+
+        methods: {
+            revertMessage() {
+                this.message = this.message.split('').reverse().join('')
+            },
+
+            atMostChars(str, size) {
+                return str.slice(0, size)
+            }
+        }
+    })
+})
